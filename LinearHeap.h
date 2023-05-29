b2339bb2577f0fff2830767d72ff6b9214c1630d
Z-fun <31031356+stevenzheng2002@users.noreply.github.com>
2023-05-29 20:31:35 +0800

    Update LinearHeap.h

--- LinearHeap.h
+++ LinearHeap.h
@@ -7,6 +7,7 @@ class ListLinearHeap {
 private:
 	ui n; // number vertices
 	ui key_cap; // the maximum allowed key value
+	ui tot;
 
 	ui min_key; // possible min key
 	ui max_key; // possible max key
@@ -20,9 +21,10 @@ private:
 	long long int max_occupy=0;
 
 public:
-	ListLinearHeap(ui _n, ui _key_cap) {
+	ListLinearHeap(ui _n, ui _key_cap, ui _tot) {
 		n = _n;
 		key_cap = _key_cap;
+		tot = _tot;
 
 		min_key = max_key = key_cap;
 
@@ -50,26 +52,26 @@ public:
 	long long get_max_occupy(){
 		return max_occupy;
 	}
-	// 线性堆初始化，_n为总数，_key_cap为最大值，id_s为下标i对应的点，key_s[i]为点i的值
 	void init(ui _n, ui _key_cap, ui *_id_s, ui *_key_s) {
-		if(key_s == nullptr) key_s = new ui[n];
-		if(pre_s == nullptr) pre_s = new ui[n];
-		if(next_s == nullptr) next_s = new ui[n];
+		if(key_s == nullptr) key_s = new ui[tot];
+		if(pre_s == nullptr) pre_s = new ui[tot];
+		if(next_s == nullptr) next_s = new ui[tot];
 		if(head_s == nullptr) head_s = new ui[key_cap+1];
 		max_occupy+=sizeof(int)*(n+n+n+key_cap+1);
 
 		assert(_key_cap <= key_cap);
 		min_key = max_key = _key_cap;
-		for(ui i = 0;i <= _key_cap;i ++) head_s[i] = n;
+		for(ui i = 0;i <= _key_cap;i ++) head_s[i] = tot;
 
 		for(ui i = 0;i < _n;i ++) {
 			ui id = _id_s[i];
 			ui key = _key_s[id];
 			if(key>_key_cap) 
-			assert(id < n); assert(key <= _key_cap);
+			// cout << key << " " << _key_cap  << " " << _n << endl;
+			assert(id < tot); assert(key <= _key_cap);
 
-			key_s[id] = key; pre_s[id] = n; next_s[id] = head_s[key];
-			if(head_s[key] != n) pre_s[head_s[key]] = id;
+			key_s[id] = key; pre_s[id] = tot; next_s[id] = head_s[key];
+			if(head_s[key] != tot) pre_s[head_s[key]] = id;
 			head_s[key] = id;
 
 			if(key < min_key) min_key = key;
@@ -77,7 +79,7 @@ public:
 	}
 
 	ui get_key(ui id) { return key_s[id]; }
-	// 从小到大存入vs中
+
 	void get_ids(ui *vs, ui &vs_size) {
 		for(ui i = min_key;i <= max_key;i ++) {
 			for(ui id = head_s[i];id != n;id = next_s[id]) {
@@ -85,7 +87,7 @@ public:
 			}
 		}
 	}
-	// 获取堆中最小值，id为最小值的点，key为最小值
+
 	bool get_min(ui &id, ui &key) {// return true if success, return false otherwise
 		while(min_key <= max_key&&head_s[min_key] == n) ++ min_key;
 		if(min_key > max_key) return false;
@@ -97,9 +99,9 @@ public:
 
 		return true;
 	}
-	// 获取最小值+删除最小值
+
 	bool pop_min(ui &id, ui &key) {// return true if success, return false otherwise
-		while(min_key <= max_key&&head_s[min_key] == n) ++ min_key;
+		while(min_key <= max_key&&head_s[min_key] == tot) ++ min_key;
 		if(min_key > max_key) return false;
 
 		id = head_s[min_key];
@@ -108,30 +110,34 @@ public:
 		assert(key_s[id] == key);
 
 		head_s[min_key] = next_s[id];
-		if(head_s[min_key] != n) pre_s[head_s[min_key]] = n;
+		if(head_s[min_key] != tot) pre_s[head_s[min_key]] = tot;
 		return true;
 	}
-	// 更新点id的值，值减去dec
-	ui decrement(ui id, ui dec) {
+
+	ui decrement(ui id, ui dec){
+		if(key_s[id] == 0){		// 等于0 直接return
+			return key_s[id];
+		}
 		if(key_s[id] < dec){
 			printf("key_s[%d]=%d,dec=%d\n", id, key_s[id], dec);
 		}
 		assert(key_s[id] >= dec);
 
-		if(pre_s[id] == n) {
+		if(pre_s[id] == tot) {
+			// printf("%d %d %d\n", id, key_s[id], head_s[key_s[id]]);
 			assert(head_s[key_s[id]] == id);
 			head_s[key_s[id]] = next_s[id];
-			if(next_s[id] != n) pre_s[next_s[id]] = n;
+			if(next_s[id] != tot) pre_s[next_s[id]] = tot;
 		}
 		else {
 			ui pid = pre_s[id];
 			next_s[pid] = next_s[id];
-			if(next_s[id] != n) pre_s[next_s[id]] = pid;
+			if(next_s[id] != tot) pre_s[next_s[id]] = pid;
 		}
 
 		ui &key = key_s[id];
-		key -= dec; pre_s[id] = n; next_s[id] = head_s[key];
-		if(head_s[key] != n) pre_s[head_s[key]] = id;
+		key -= dec; pre_s[id] = tot; next_s[id] = head_s[key];
+		if(head_s[key] != tot) pre_s[head_s[key]] = id;
 		head_s[key] = id;
 
 		if(key < min_key) min_key = key;
