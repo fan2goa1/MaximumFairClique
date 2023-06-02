0d030e5809081ff1781cc08f78e877fa9ee79251
Z-fun <31031356+stevenzheng2002@users.noreply.github.com>
2023-06-02 16:24:15 +0800

    Fix some bugs.

--- Graph_baseline.cpp
+++ Graph_baseline.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-void Graph::MaxRFClique(){
-    MRFC_real = MRFC_heu;       // 用于存储真正的MRFC
+void Graph::MaxRFClique(){  
+    for(auto u : MRFC_heu) MRFC_real.push_back(u);      // 用于存储真正的MRFC
     int* vis = new int[n];
     nvis = new int[n];
     for(int i = 0; i < n; i ++) nvis[i] = 0;
@@ -31,39 +31,48 @@ void Graph::MaxRFClique(){
             }
             // for(auto u : candidates[0]) printf("%d ", u); puts("");
             // for(auto u : candidates[1]) printf("%d ", u); puts("");
-            Branch(R, candidates, candidate_siz, X, att_cnt, -1, 0);
+            Branch(R, candidates, candidate_siz, att_cnt, -1, 0);
         }
     }
 }
 
-void Graph::Branch(vector<int> &R, vector<int>* candidates, int candidate_siz, vector<int> X, vector<int> att_cnt, int attr_max, int tar_attr){
+void Graph::Branch(vector<int> &R, vector<int>* candidates, int candidate_siz, vector<int> att_cnt, int attr_max, int tar_attr){
     if(attr_max == -1 && candidates[tar_attr].size() == 0){     // 说明该属性无法继续扩展，设置attr_max
-		attr_max = delta + att_cnt[tar_attr];
-        Branch(R, candidates, candidate_siz, X, att_cnt, attr_max, tar_attr ^ 1);
+        attr_max = delta + att_cnt[tar_attr];
+        Branch(R, candidates, candidate_siz, att_cnt, attr_max, 1-tar_attr);
         return ;
 	}
     // cout << "Now R size: " << R.size() << endl;
 	if(candidate_siz == 0 || (attr_max != -1 && att_cnt[tar_attr] >= attr_max)){ // 候选集为空或者封顶了
 		int toPrint = true;
-		if(X.size() != 0){
-			//如果加一个进去，却不会破坏它的公平性，就不是答案
-			int attr_min = attr_max - delta;
-			for(int i = 0; i < X.size(); i ++){
-				if(att_cnt[attribute[X[i]]]==attr_min || att_cnt[attribute[X[i]]] + 1 <= attr_max){
-					toPrint = false;
-                    break;
-				}
-			}
-		}
-		if(toPrint){
-            // printf("Found one! Size: %d; Now max size: %d\n", R.size(), MRFC_real.size());
-            if(R.size() >= MRFC_real.size()) MRFC_real = R;
+		// if(X.size() != 0){
+		// 	//如果加一个进去，却不会破坏它的公平性，就不是答案
+		// 	int attr_min = attr_max - delta;
+		// 	for(int i = 0; i < X.size(); i ++){
+		// 		if(att_cnt[attribute[X[i]]]==attr_min || att_cnt[attribute[X[i]]] + 1 <= attr_max){
+		// 			toPrint = false;
+        //             break;
+		// 		}
+		// 	}
+		// }
+		if(toPrint && R.size() > MRFC_real.size() || !have_ans){        // update
+            cout << "Found one! Size:" << R.size() << "; Now max size: " << MRFC_real.size() << endl;
+            // int cnta = 0, cntb = 0;
+            // for(auto u : R){
+            //     if(attribute[u] == 0) cnta ++;
+            //     else cntb ++;
+            // }
+            // cout << cnta << " " << cntb << endl;
+            for(auto u : R) cout << u << " "; cout << endl;
+            cout.flush();
+            MRFC_real = R;
+            have_ans = 1;
 		}
         return ;
 	}
 
     if(candidates[tar_attr].size() == 0){   // 说明该属性无法继续扩展，同时attr_max已经被设置，且没有达到封顶
-        Branch(R, candidates, candidate_siz, X, att_cnt, attr_max, tar_attr ^ 1);
+        Branch(R, candidates, candidate_siz, att_cnt, attr_max, 1-tar_attr);
         return ;
     }
 
@@ -105,9 +114,14 @@ void Graph::Branch(vector<int> &R, vector<int>* candidates, int candidate_siz, v
             }
 		}
 
-        // bool cut_flag = calc_ub(newR, newC);
-        bool cut_flag = calc_ub(R, newC);
-        // cout << cut_flag << endl;
+        bool cut_flag = true;
+        if(this->alg_type == "ub" || this->alg_type == "heur"){     // ub tech
+            cut_flag = calc_ub(R, newC);
+        }
+        else if(this->alg_type == "base"){  // baseline, only examine the size
+            cut_flag = calc_base(R, newC);
+        }
+
         if(cut_flag == false){       // 被剪枝
             // printf("Cut! Now size: %d\n", R.size());
             // newR.pop_back();
@@ -118,18 +132,19 @@ void Graph::Branch(vector<int> &R, vector<int>* candidates, int candidate_siz, v
             continue;
         }
 		
-        vector<int> newX; newX.clear();
-		for(int j = 0; j < X.size(); j ++){
-            if(nvis[X[j]]) newX.push_back(X[j]);
-		}
+        // vector<int> newX; newX.clear();
+		// for(int j = 0; j < X.size(); j ++){
+        //     if(nvis[X[j]]) newX.push_back(X[j]);
+		// }
 		/**recover**/
 		for(int j = offset[cur]; j < pend[cur]; j ++){
             nvis[edge_list[j]] = 0;
 		}
 		/****/
 		// Branch(newR, newC, candidate_siz, newX, att_cnt, attr_max, tar_attr ^ 1);
-        Branch(R, newC, candidate_siz, newX, att_cnt, attr_max, tar_attr ^ 1);
-		X.push_back(cur);
+        att_cnt[tar_attr] ++;
+        Branch(R, newC, candidate_siz, att_cnt, attr_max, 1-tar_attr);
+		// X.push_back(cur);
 		// newR.pop_back();
         R.pop_back();
 	}
